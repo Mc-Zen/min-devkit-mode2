ae0bf3713b322ccdd0cfd823ad3efa464766bba8
Mc-zen <alphabetagamma11@hotmail.com>
2022-10-12 14:49:57 +0200

    add and update cmake scripts

---
+++ source/min-api/script/min-api.cmake
@@ -0,0 +1,7 @@
+# Copyright 2018 The Min-API Authors. All rights reserved.
+# Use of this source code is governed by the MIT License found in the License.md file.
+
+include("${CMAKE_CURRENT_LIST_DIR}/add_min_target.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/add_max_target.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/macros.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/git-rev.cmake")
